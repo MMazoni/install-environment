84ca5f63c81c328a185da847aadad2b460ee883b
Matheus Mazoni <mmazoni.andrade@gmail.com>
2021-06-22 23:37:39 -0300

    add vim config

--- install.sh
+++ install.sh
@@ -6,12 +6,18 @@ echo "Choose [u] for Ubuntu and [d] for Debian:"
 read input
 
 sudo apt update
+sudo apt upgrade -y
 sudo apt install git curl fish wget -y
 
 # fish
 mkdir ~/.config/fish
 curl -o ~/.config/fish/config.fish https://raw.githubusercontent.com/MMazoni/configuration-files/master/.config/fish/config.fish
 sudo chsh -s $(which fish) $USER
+
+# vim
+
+curl -o ~/.vimrc https://raw.githubusercontent.com/MMazoni/configuration-files/master/.vimrc
+curl -o ~/.vim/colors/atlantic-dark.vim https://raw.githubusercurrent.com/MMazoni/configuration-files/master/.vim/colors/atlantic-dark.vim --create-dirs
 
 # github
 git config --global user.name "Matheus Mazoni"
@@ -22,4 +28,6 @@ eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 sshcode=`cat ~/.ssh/id_rsa.pub`
 echo $sshcode
+read -p "Press enter to continue"
+echo "Teste"
 
